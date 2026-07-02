5e7200aa4352f4eeb16ce04bff173e2f9502b233
Ryan-Iago <ryan.andre.prado@escola.pr.gov.br>
2026-07-02 11:05:23 -0300

    Add random password generation to card creation

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,33 @@
+function gerarSenhaAleatoria(tamanho) {
+    const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=';
+    let senha = '';
+    for (let i = 0; i < tamanho; i++) {
+        const indiceAleatorio = Math.floor(Math.random() * caracteres.length);
+        senha += caracteres.charAt(indiceAleatorio);
+    }
+    return senha;
+}
+
 criaCartao(
-    'História',
-    'Como era conhecido o Século 18?',
-    'Era conhecido como Século das luzes'
+    'Sugestão de senha forte 1',
+    'senha 50 Caracteres',
+    gerarSenhaAleatoria(50)
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da Rússia',
-    'A capital da Rússia é Moscou'
+    'sugestão de senha forte 2',
+    'senha 25 caracteres',
+    gerarSenhaAleatoria(25)
 )
 
 criaCartao(
     'Programação',
     'Como se abre uma variavel em um projeto javascript',
-    'Utilizando o comando "IF" na programação'
+    'Utilizando as palavras-chave "let", "const" ou "var"'
 )
 
 criaCartao(
-    'Inglês',
-    'Qual é a tradução de "Good afternoon"?',
-    'A tradução de "Good afternoon" é "Boa tarde".'
+    'Sugestão de senha forte 3',
+    'senha 15 Caracteres',
+    gerarSenhaAleatoria(15)
 )
